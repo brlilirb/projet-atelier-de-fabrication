8f5481a07fb6675bbca527cfca834ff867264dc4
lili <lili.bourgeois-romain@insa-strasbourg.fr>
2025-03-24 17:00:00 +0100

    creation de la classe operation

---
+++ src/main/java/INSA/TD/Operation.java
@@ -0,0 +1,9 @@
+package INSA.TD;
+
+public class Operation {
+    private String refOperation;
+    private String dOperation;
+    private String refequipement;
+    private float dureeOperation; //unite : heure
+
+}
