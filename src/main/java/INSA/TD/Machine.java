95db461b7139a6469b33ee7c55a07ddee66d4cb2
lili <lili.bourgeois-romain@insa-strasbourg.fr>
2025-03-24 16:32:58 +0100

    creation de la classe

---
+++ src/main/java/INSA/TD/Machine.java
@@ -0,0 +1,10 @@
+package INSA.TD;
+
+public class Machine {
+    private String refMachine;
+    private String dMachine;
+    private String type;
+    private float cout;
+    private float abscisse;
+    private float ordonnee;
+}
