dea3cf3c44420b39e2bb0750e32f4a6f05892be6
clement80333 <plumearthure@gmail.com>
2025-04-02 11:29:47 +0200

    classe lire + parti classe Poste a finir classe modifier poste

--- src/main/java/INSA/TD/Poste.java
+++ src/main/java/INSA/TD/Poste.java
@@ -14,12 +14,44 @@ public class Poste extends Equipement {
     }
 
     public void modifierPoste(){
-        double n;
+        int n;
         System.out.println("Que voulez-modifier ?");
         System.out.println("1: la référence");
         System.out.println("2: la description");
         System.out.println("3: la liste des machines");
-        n = Lire.d();
+        System.out.println("0: pour revenir en arrière");
+        n = Lire.i();
+        switch (n){
+            case 1 : {
+                System.out.println("Donnez la nouvelle référence");
+                this.refPoste = Lire.S();
+                break;
+            }
+            case 2 : {
+                System.out.println("Donnez la nouvelle description");
+                this.dPoste = Lire.S();
+                break;
+            }
+            case 3 : {
+                System.out.println("Voulez-vous :");
+                System.out.println("1 : ajouter une machine");
+                System.out.println("2 : supprimer une machine");
+                n=Lire.i();
+                switch(n){
+                    case 1:{
+                        System.out.print("Quel machine voulez-vous ajoutez?");
+                        //listeMachines.add(Lire.Machine());
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("Quel machine voulez-vous retirer?");
+                            //
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 
     public void afficherPoste(){
