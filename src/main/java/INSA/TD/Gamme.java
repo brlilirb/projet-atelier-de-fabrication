9a3594ba82eb23006f7a2ecc1b5e62e665ac66d1
clement80333 <plumearthure@gmail.com>
2025-04-02 10:21:24 +0200

    classe Gamme constructeur

--- src/main/java/INSA/TD/Gamme.java
+++ src/main/java/INSA/TD/Gamme.java
@@ -7,8 +7,33 @@ public class Gamme {
     private ArrayList<Operation> listeOperations = new ArrayList<>();
     private ArrayList<Equipement> listeEquipements = new ArrayList<>();
 
-    public Gamme (){
-
+    public Gamme (String ref, ArrayList<Operation> ope, ArrayList<Equipement> equipement){
+    ref=this.refGamme;
+    ope=this.listeOperations;
+    equipement=this.listeEquipements;
     }
 
+    public String getRefGamme() {
+        return refGamme;
+    }
+
+    public void setRefGamme(String refGamme) {
+        this.refGamme = refGamme;
+    }
+
+    public ArrayList<Operation> getListeOperations() {
+        return listeOperations;
+    }
+
+    public void setListeOperations(ArrayList<Operation> listeOperations) {
+        this.listeOperations = listeOperations;
+    }
+
+    public ArrayList<Equipement> getListeEquipements() {
+        return listeEquipements;
+    }
+
+    public void setListeEquipements(ArrayList<Equipement> listeEquipements) {
+        this.listeEquipements = listeEquipements;
+    }
 }
