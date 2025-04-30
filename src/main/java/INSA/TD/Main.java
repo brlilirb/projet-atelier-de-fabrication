e033e50f6d59d9d4a043220e305a49659a5450d1
lili <lili.bourgeois-romain@insa-strasbourg.fr>
2025-04-30 20:49:32 +0200

    ajout de javafx au projet, tout fonctionne

--- src/main/java/INSA/TD/Main.java
+++ src/main/java/INSA/TD/Main.java
@@ -1,47 +1,24 @@
 package INSA.TD;
 
-import INSA.TD.controllers.InitController;
-import INSA.TD.controllers.implementation.InitControllerImpl;
+import INSA.TD.views.implementation.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch();
+    }
 
-        InitController initController = InitControllerImpl.getInstance();
+    @Override
+    public void start(Stage stage) throws Exception {
+        MainView view = new MainView();
+        Scene scene = new Scene(view.getView(), 400, 300);
 
-        /*MaintenanceServiceImpl maintenanceService = MaintenanceServiceImpl.getInstance();
-        maintenanceService.addStringEvent("2025-05-10 10:23 mach_2 A");
-        maintenanceService.addStringEvent("2025-05-10 12:15 mach_2 D");
-        maintenanceService.addStringEvent("2025-05-10 18:41 mach_2 A");
-        maintenanceService.addStringEvent("2025-05-10 19:41 mach_2 D");
-        maintenanceService.addStringEvent("2025-05-12 14:16 mach_2 A");
-        maintenanceService.addStringEvent("2025-05-10 17:31 mach_2 D");
-
-        maintenanceService.addStringEvent("2025-05-10 11:23 mach_1 A");
-        maintenanceService.addStringEvent("2025-05-10 12:15 mach_1 D");
-        maintenanceService.addStringEvent("2025-05-10 18:41 mach_1 A");
-        maintenanceService.addStringEvent("2025-05-10 21:41 mach_1 D");
-        maintenanceService.addStringEvent("2025-05-12 15:16 mach_1 A");
-        maintenanceService.addStringEvent("2025-05-13 17:31 mach_1 D");
-
-        Machine mach_1 = new Machine("soudeuse", 2, 3, 300, "soudeuse3000", "mach_1");
-        Machine mach_2 = new Machine("soudeuse", 2, 3, 300, "soudeuse3000", "mach_2");
-
-        MachineService machineService = MachineServiceImpl.getInstance();
-
-        machineService.add(mach_1);
-        machineService.add(mach_2);
-
-        */
-
-        initController.load();
-        initController.save();
-        /*MachineService machineService = MachineServiceImpl.getInstance();
-
-        MaintenanceServiceImpl maintenanceService = MaintenanceServiceImpl.getInstance();
-
-        maintenanceService.computeAllFiabilites();*/
-
-       /* MachineController machineController = MachineControllerImpl.getInstance();
-        machineController.afficherTous();*/
+        stage.setTitle("JavaFX MVC without FXML");
+        stage.setScene(scene);
+        stage.show();
+        /*InitController initController = new AtelierControllerImpl();
+        initController.init();*/
     }
 }
