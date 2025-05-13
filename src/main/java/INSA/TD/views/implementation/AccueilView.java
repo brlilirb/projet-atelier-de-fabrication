9c3efe81dc7a775cecd4e60f987af93beb49e476
Adil <adil.cherki2006@gmail.com>
2025-05-13 14:34:43 +0200

    ajout de la page d'accueil

--- src/main/java/INSA/TD/views/implementation/AccueilView.java
+++ src/main/java/INSA/TD/views/implementation/AccueilView.java
@@ -1,4 +1,4 @@
-package INSA.TD;
+package INSA.TD.views.implementation;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -6,25 +6,31 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.application.Application;
 
-public class Main extends Application {
+public class AccueilView {
 
     public void start(Stage stage) {
+        // Titre de la page
         Label titre = new Label("Quelles sont vos autorisations ?");
+
+        // Création des boutons
         Button btnOuvrier = new Button("Ouvrier");
         Button btnChef = new Button("Chef d'atelier");
 
+        // Actions pour les boutons
         btnOuvrier.setOnAction(_ -> System.out.println("Ouvrier sélectionné"));
         btnChef.setOnAction(_ -> System.out.println("Chef d'atelier sélectionné"));
 
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(titre, btnOuvrier, btnChef);
-        layout.setAlignment(Pos.CENTER);
+        // Mise en page horizontale pour les boutons
+        VBox layout = new VBox(20);  // 20 pixels d'écart entre les boutons
+        layout.getChildren().addAll(btnOuvrier, btnChef);
+        layout.setAlignment(Pos.CENTER);  // Centre les boutons horizontalement
 
+        // Scène
         Scene scene = new Scene(layout, 400, 200);
         stage.setTitle("Page d'accueil");
         stage.setScene(scene);
         stage.show();
     }
 }
+
