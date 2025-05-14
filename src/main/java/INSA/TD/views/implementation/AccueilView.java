591bf728d70adf030556269902a5da407f92e624
Adil <adil.cherki2006@gmail.com>
2025-05-14 10:17:49 +0200

    ajout de la page d'accueil et correction main

--- src/main/java/INSA/TD/views/implementation/AccueilView.java
+++ src/main/java/INSA/TD/views/implementation/AccueilView.java
@@ -10,23 +10,18 @@ import javafx.stage.Stage;
 public class AccueilView {
 
     public void start(Stage stage) {
-        // Titre de la page
+        // Création de la vue Accueil
         Label titre = new Label("Quelles sont vos autorisations ?");
-
-        // Création des boutons
         Button btnOuvrier = new Button("Ouvrier");
         Button btnChef = new Button("Chef d'atelier");
 
-        // Actions pour les boutons
         btnOuvrier.setOnAction(_ -> System.out.println("Ouvrier sélectionné"));
         btnChef.setOnAction(_ -> System.out.println("Chef d'atelier sélectionné"));
 
-        // Mise en page horizontale pour les boutons
-        VBox layout = new VBox(20);  // 20 pixels d'écart entre les boutons
-        layout.getChildren().addAll(btnOuvrier, btnChef);
-        layout.setAlignment(Pos.CENTER);  // Centre les boutons horizontalement
+        VBox layout = new VBox(20);
+        layout.getChildren().addAll(titre, btnOuvrier, btnChef);
+        layout.setAlignment(Pos.CENTER);
 
-        // Scène
         Scene scene = new Scene(layout, 400, 200);
         stage.setTitle("Page d'accueil");
         stage.setScene(scene);
