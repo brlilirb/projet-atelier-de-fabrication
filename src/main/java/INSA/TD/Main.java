158b250b86ba41f731a7bb841ab46f1529a576c2
Adil <adil.cherki2006@gmail.com>
2025-05-14 10:17:49 +0200

    ajout de la page d'accueil et correction main

--- src/main/java/INSA/TD/Main.java
+++ src/main/java/INSA/TD/Main.java
@@ -1,30 +1,19 @@
 package INSA.TD;
 
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.stage.Stage;
+import INSA.TD.views.implementation.AccueilView;
 
 public class Main extends Application {
 
-    public void start(Stage stage) {
-        Label titre = new Label("Quelles sont vos autorisations ?");
-        Button btnOuvrier = new Button("Ouvrier");
-        Button btnChef = new Button("Chef d'atelier");
+    @Override
+    public void start(Stage primaryStage) {
+        // Appel de AccueilView pour afficher l'interface
+        AccueilView accueilView = new AccueilView();
+        accueilView.start(primaryStage);  // Affichage de la vue Accueil
+    }
 
-        btnOuvrier.setOnAction(_ -> System.out.println("Ouvrier sélectionné"));
-        btnChef.setOnAction(_ -> System.out.println("Chef d'atelier sélectionné"));
-
-        VBox layout = new VBox(20);
-        layout.getChildren().addAll(titre, btnOuvrier, btnChef);
-        layout.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(layout, 400, 200);
-        stage.setTitle("Page d'accueil");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);  // Lance l'application JavaFX
     }
 }
