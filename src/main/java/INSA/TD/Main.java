91965df0a0690ec080c34a68e3240af2533193ae
clement80333 <plumearthure@gmail.com>
2025-05-22 16:53:41 +0200

    nettoyage

--- src/main/java/INSA/TD/Main.java
+++ src/main/java/INSA/TD/Main.java
@@ -1,11 +1,20 @@
 package INSA.TD;
 
 import INSA.TD.controllers.InitController;
+import INSA.TD.controllers.MachineController;
+import INSA.TD.controllers.PosteController;
 import INSA.TD.controllers.implementation.InitControllerImpl;
+import INSA.TD.controllers.implementation.MachineControllerImpl;
+import INSA.TD.controllers.implementation.PosteControllerImpl;
+import INSA.TD.models.Machine;
+import INSA.TD.models.Poste;
+import INSA.TD.models.etat.machine.EtatMachine;
 import INSA.TD.utils.ConstantesUtils;
 import INSA.TD.views.AccueilView;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class Main extends Application {
     @Override
