52223c0b317448f4a8f96f3c57b977c7b13235eb
clement80333 <plumearthure@gmail.com>
2025-05-23 01:13:11 +0200

    poste view sans créer

--- src/main/java/INSA/TD/views/entity/PosteView.java
+++ src/main/java/INSA/TD/views/entity/PosteView.java
@@ -5,19 +5,37 @@ import INSA.TD.models.Poste;
 import INSA.TD.views.entity.TableauPosteView;
 import INSA.TD.views.entity.MachineView;
 import INSA.TD.views.entity.PosteView;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import org.w3c.dom.Text;
+import javafx.scene.control.TextField;
+
 
 import java.util.List;
 
-public class PosteView extends HBox {
+public class PosteView extends Pane {
 
     private final TableauPosteView posteTableView = new TableauPosteView();
     private final MachineView machineView = new MachineView();
+    private Label listePoste=new Label("liste des postes :");
+    private Label listMachine=new Label("liste des machines de ce poste");
 
     public PosteView() {
-        setSpacing(20);
-        getChildren().addAll(posteTableView, machineView);
+        // position des tableaux
+        posteTableView.setLayoutX(20);
+        posteTableView.setLayoutY(45);
 
+        machineView.setLayoutX(450);
+        machineView.setLayoutY(45);
+
+        listePoste.setLayoutX(20);
+        listePoste.setLayoutY(20);
+
+        listMachine.setLayoutX(450);
+        listMachine.setLayoutY(20);
+
+        getChildren().addAll(posteTableView,listePoste,listMachine, machineView);
         posteTableView.setOnVoirClicked(this::handleVoirMachinesDuPoste);
     }
 
--- src/main/java/INSA/TD/views/entity/TableauPosteView.java
+++ src/main/java/INSA/TD/views/entity/TableauPosteView.java
@@ -3,9 +3,11 @@ package INSA.TD.views.entity;
 import INSA.TD.controllers.PosteController;
 import INSA.TD.controllers.implementation.PosteControllerImpl;
 import INSA.TD.models.Poste;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
 
 import java.util.function.Consumer;
@@ -24,14 +26,13 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
         // Ajoute les colonnes spécifiques du poste ici (ex: nom, type...)
 
         TableColumn<Poste, Void> voirCol = new TableColumn<>("Machines");
+        TableColumn<Poste, String> descriptionCol = initDescriptionColumn();
 
         voirCol.setCellFactory(new Callback<>() {
             @Override
             public TableCell<Poste, Void> call(final TableColumn<Poste, Void> param) {
                 return new TableCell<>() {
-
                     private final Button btn = new Button("Voir");
-
                     {
                         btn.setOnAction(event -> {
                             Poste poste = getTableView().getItems().get(getIndex());
@@ -40,7 +41,6 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
                             }
                         });
                     }
-
                     @Override
                     protected void updateItem(Void item, boolean empty) {
                         super.updateItem(item, empty);
@@ -50,7 +50,15 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
             }
         });
 
-        getTableView().getColumns().add(voirCol);
+        getTableView().getColumns().addAll(descriptionCol,voirCol);
+    }
+
+    protected TableColumn<Poste, String> initDescriptionColumn() {
+        TableColumn<Poste, String> descriptionCol = new TableColumn<>("Description");
+        descriptionCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDesignation()));
+        descriptionCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        descriptionCol.setOnEditCommit(event -> event.getRowValue().setDesignation(event.getNewValue()));
+        return descriptionCol;
     }
 
     @Override
