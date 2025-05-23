fe037fb9ef078273dd9c86ff73c58a09e9ea1fb7
clement80333 <plumearthure@gmail.com>
2025-05-23 21:09:57 +0200

    poste view avec bouton créer et supprimer

--- src/main/java/INSA/TD/views/entity/PosteView.java
+++ src/main/java/INSA/TD/views/entity/PosteView.java
@@ -1,17 +1,20 @@
 package INSA.TD.views.entity;
 
+import INSA.TD.controllers.implementation.AbstractEntityController;
 import INSA.TD.models.Machine;
 import INSA.TD.models.Poste;
+import INSA.TD.views.button.AddButton;
+import INSA.TD.controllers.implementation.PosteControllerImpl;
 import INSA.TD.views.entity.TableauPosteView;
 import INSA.TD.views.entity.MachineView;
 import INSA.TD.views.entity.PosteView;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import org.w3c.dom.Text;
-import javafx.scene.control.TextField;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosteView extends Pane {
@@ -20,22 +23,35 @@ public class PosteView extends Pane {
     private final MachineView machineView = new MachineView();
     private Label listePoste=new Label("liste des postes :");
     private Label listMachine=new Label("liste des machines de ce poste");
+    private Label addPosteText=new Label("ajouter un poste");
+    private AddButton add=new AddButton();
 
     public PosteView() {
-        // position des tableaux
-        posteTableView.setLayoutX(20);
-        posteTableView.setLayoutY(45);
+        // position des objets
+        posteTableView.setLayoutX(20);posteTableView.setLayoutY(45);
 
-        machineView.setLayoutX(450);
-        machineView.setLayoutY(45);
+        machineView.setLayoutX(450);machineView.setLayoutY(45);
 
-        listePoste.setLayoutX(20);
-        listePoste.setLayoutY(20);
+        listePoste.setLayoutX(20);listePoste.setLayoutY(20);
 
-        listMachine.setLayoutX(450);
-        listMachine.setLayoutY(20);
+        listMachine.setLayoutX(450);listMachine.setLayoutY(20);
 
-        getChildren().addAll(posteTableView,listePoste,listMachine, machineView);
+        addPosteText.setLayoutX(20);addPosteText.setLayoutY(500);
+
+        add.setLayoutX(120);
+        add.setLayoutY(500);
+        add.setOnAction(event -> {
+            List<Machine> nouveauMachine= new ArrayList<>();
+            nouveauMachine.add(new Machine("",0,0,0,"","nouveau"));
+            Poste poste=new Poste(nouveauMachine,"","nouveau");
+            poste.setProperties("","nouveau");
+            System.out.println(poste);
+            PosteControllerImpl.getInstance().ajouter(poste);
+            posteTableView.updateData(PosteControllerImpl.getInstance().afficherTous());
+
+        });
+
+        getChildren().addAll(posteTableView,listePoste,listMachine,addPosteText,add, machineView);
         posteTableView.setOnVoirClicked(this::handleVoirMachinesDuPoste);
     }
 
--- src/main/java/INSA/TD/views/entity/TableauPosteView.java
+++ src/main/java/INSA/TD/views/entity/TableauPosteView.java
@@ -1,15 +1,21 @@
 package INSA.TD.views.entity;
 
 import INSA.TD.controllers.PosteController;
+import INSA.TD.controllers.implementation.AbstractEntityController;
 import INSA.TD.controllers.implementation.PosteControllerImpl;
 import INSA.TD.models.Poste;
+import INSA.TD.views.menu.MenuView;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.util.Callback;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 
@@ -24,13 +30,55 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
     @Override
     protected void initSpecificTableColumns() {
         // Ajoute les colonnes spécifiques du poste ici (ex: nom, type...)
-
-        TableColumn<Poste, Void> voirCol = new TableColumn<>("Machines");
+        getTableView().setEditable(MenuView.isAutorisation());
+        TableColumn<Poste, Void> voirCol = initVoirColumn();
         TableColumn<Poste, String> descriptionCol = initDescriptionColumn();
+        getTableView().getColumns().addAll(descriptionCol,voirCol);
+        if(MenuView.isAutorisation()==true){
+            TableColumn<Poste,Void> supprimerCol=initSupprimerColumn();
+            getTableView().getColumns().add(supprimerCol);
+        }
+    }
 
+    protected TableColumn<Poste, Void> initSupprimerColumn() {
+        TableColumn<Poste, Void> supprimerCol = new TableColumn<>("Supprimer");
+
+        supprimerCol.setCellFactory(param -> new TableCell<>() {
+            private final Button deleteButton = new Button();
+            private final ImageView icon = new ImageView(
+                    new Image(getClass().getResourceAsStream("/Image/moins.png"))
+            );
+
+            {
+                icon.setFitHeight(16);
+                icon.setFitWidth(16);
+                icon.setPreserveRatio(true);
+
+                deleteButton.setGraphic(icon);
+                deleteButton.setStyle("-fx-background-color: transparent;"); // transparent pour n’afficher que l’image
+                deleteButton.setOnAction(event -> {
+                    Poste poste = getTableView().getItems().get(getIndex());
+                    PosteControllerImpl.getInstance().supprimer(poste.getId());
+                    updateData(PosteControllerImpl.getInstance().afficherTous());
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                setGraphic(empty ? null : deleteButton);
+            }
+        });
+
+        return supprimerCol;
+    }
+
+
+    protected TableColumn<Poste,Void> initVoirColumn(){
+        TableColumn<Poste, Void> voirCol = new TableColumn<>("Machines");
         voirCol.setCellFactory(new Callback<>() {
             @Override
-            public TableCell<Poste, Void> call(final TableColumn<Poste, Void> param) {
+            public TableCell<Poste, Void> call(final TableColumn<Poste, Void> param) {//définir qu'il y a un boutton dans les cellules
                 return new TableCell<>() {
                     private final Button btn = new Button("Voir");
                     {
@@ -41,7 +89,7 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
                             }
                         });
                     }
-                    @Override
+                    @Override // permet d'afficher le bouton que si il y a un poste sur la ligne
                     protected void updateItem(Void item, boolean empty) {
                         super.updateItem(item, empty);
                         setGraphic(empty ? null : btn);
@@ -49,20 +97,30 @@ public class TableauPosteView extends AbstractEntityView<Poste> {
                 };
             }
         });
-
-        getTableView().getColumns().addAll(descriptionCol,voirCol);
+        return  voirCol;
     }
 
     protected TableColumn<Poste, String> initDescriptionColumn() {
         TableColumn<Poste, String> descriptionCol = new TableColumn<>("Description");
         descriptionCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDesignation()));
         descriptionCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        descriptionCol.setOnEditCommit(event -> event.getRowValue().setDesignation(event.getNewValue()));
+        descriptionCol.setOnEditCommit(event -> {
+            event.getRowValue().setDesignation(event.getNewValue());
+            getTableView().refresh();
+        });
+
+
         return descriptionCol;
     }
+
+    public void updateData(List<Poste> postes) {
+        getTableView().getItems().setAll(postes);
+    }
+
 
     @Override
     protected PosteController getController() {
         return PosteControllerImpl.getInstance();
     }
+
 }
